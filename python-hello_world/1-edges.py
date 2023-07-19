bc75097ff7e8033b4fb12c4e6495d7c0f8ddba41
muhe131 <mosmn131@gmail.com>
2023-07-19 09:22:10 +0300

    1-pyt

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_word = "Hol"
-last_word = "on"
-middle_word = "olberto"
+first_word = "Sch"
+last_word = "ol"
+middle_word = "choo"
 print("First 3 letters: {}".format(first_word))
 print("Last 2 letters: {}".format(last_word))
 print("Middle word: {}".format(middle_word))
