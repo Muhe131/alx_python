c14485fa0afcd75e6b6c9a7b3f55319d9bfa16d0
muhe131 <mosmn131@gmail.com>
2023-07-23 16:06:28 +0300

    3-pyth

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def reverse_string(string):
-     reverse  =  ' ' #declaring empty string to store the reversed string
-     for i in range ( len(string),  0, -1):
-          reverse += string[i  - 1]
-          return reverse
+     string = string[::-1]
+     return string
