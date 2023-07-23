fb93d3f50f298aab25700bb5a4022e795ae7936d
muhe131 <mosmn131@gmail.com>
2023-07-23 17:22:19 +0300

    pass

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -2,4 +2,4 @@ from math import sqrt
 def is_prime(number):
      if number<=1 or (number % 2 == 0 and number >2):
           return False
-     return all (number % i for i in range(3, int(sqrt(number)) + 1, 2))
+     return all (number % i for i in range(3, int(sqrt(number)) + 1, 2)) 
