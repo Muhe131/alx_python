cbfae9fce65fa0c840e6b90100c03bf4240ebb3f
muhe131 <mosmn131@gmail.com>
2023-07-30 21:33:01 +0300

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" This module discusses about square """
 class Square:
         """ Defines a class of square  """
         def __init__(self, size):
                 self.size = size
-my_square = Square(3)
+my_square = Square()
