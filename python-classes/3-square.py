6059bfcda0dae4165d71570d7ffa6de056078ee4
muhe131 <mosmn131@gmail.com>
2023-08-01 01:38:31 +0300

    3-square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -24,6 +24,7 @@ class Square:
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -31,8 +32,9 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         """returns the current area of the square.
             """
         return(self.__size * self.__size)
+    
