0d2e324a4de327a376edd37cc460783c6079eac9
muhe131 <mosmn131@gmail.com>
2023-08-05 15:33:05 +0300

    square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -10,9 +10,11 @@ class Square(Rectangle):
         super().integer_validator("size", size)
         self.__size = size
     def area(self):
+        
         """ Returns the square of the rectangle """
         return self.__size * self.__size
     def __str__(self):
+
         """ Returns the print() and str() representation of rectangle """
         string = "[" + str(self.__class__.__name__) + "]"
         string += str(self.__size) + "/" + str(self.__size)
