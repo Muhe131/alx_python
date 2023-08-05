349e7cc298f806da9f35c2a14f2a27d43401dbe7
muhe131 <mosmn131@gmail.com>
2023-08-05 10:39:50 +0300

    inherited

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 """ Checks if an object is inherited """
 def inherits_from(obj, a_class):
+    """ Inherited from a class """
     return (issubclass(type(obj), a_class) and type(obj) != a_class )
