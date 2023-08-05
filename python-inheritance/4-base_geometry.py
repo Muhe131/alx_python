8e73e22032040ebbd375bcc895a875aa54cf14bc
muhe131 <mosmn131@gmail.com>
2023-08-05 11:07:58 +0300

    geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" An empty class """
+class BaseGeometry:
+    """ defines base geometry class """
+    def area(self):
+        """ method not implemented yet """
+        raise Exception("area() is not implemented")
