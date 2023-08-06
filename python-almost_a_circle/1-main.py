b45213a9a733a7bb1b68b1e638487b0ba97b4d07
muhe131 <mosmn131@gmail.com>
2023-08-06 12:46:33 +0300

    ok

--- python-almost_a_circle/1-main.py
+++ python-almost_a_circle/1-main.py
@@ -6,12 +6,9 @@ if __name__ == "__main__":
 
     r1 = Rectangle(10, 2)
     print(r1.id)
-    print("OK", end="")
 
     r2 = Rectangle(2, 10)
     print(r2.id)
-    print("OK", end="")
 
     r3 = Rectangle(10, 2, 0, 0, 12)
     print(r3.id)
-    print("OK", end="")
