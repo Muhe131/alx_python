c243116d4f37311b6c56d82d57a7f05e9aa1ad51
muhe131 <mosmn131@gmail.com>
2023-08-06 09:22:43 +0300

    GEOMETRY

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 """ An empty class """
-class BaseGeometry:
-    """ defines empty class """
+class BaseGeometryMetaClass(type):
+     """ defines empty class """
+     def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     pass
+
+""" class BaseGeometry:
+     def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+class MuheClass(metaclass=BaseGeometryMetaClass):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+pass """
