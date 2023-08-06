7635bc258afd9f4db29271a0edc235000af20167
muhe131 <mosmn131@gmail.com>
2023-08-06 11:04:36 +0300

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ A base class for the upcoming projects """
 class Base:
+    """ base class """
     __nb_objects = 0
     def __init__(self, id=None):
-        """  """
+        """  initializes id"""
         if id is not None:
             self.id = id
         else:
