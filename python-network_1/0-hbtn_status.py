6627ec47fa8a546294ab3f2ce72c7f1f09e00a8f
muhe131 <mosmn131@gmail.com>
2023-08-09 23:14:33 +0300

    python networks

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,9 +5,8 @@ import requests
 
 if __name__ == "__main__":
     request = requests.Request("https://alx-intranet.hbtn.io/status")
-   #with requests.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
